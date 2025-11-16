2cc626435c5373b5d64b174b9d1d6aecf9457653
Sam Kimbinyi <sam@kimbinyi.com>
2025-11-16 17:53:50 +0000

    store trait

--- crates/skore-core/src/store.rs
+++ crates/skore-core/src/store.rs
@@ -1,6 +1,6 @@
 pub trait Store: Send + Sync {
-    fn get() -> ();
-    fn set() -> ();
-    fn delete() -> ();
-    fn flush() -> ();
+    fn get(&self, key: &[u8]) -> Result<Vec<u8>>;
+    fn set(&mut self, key: Vec<u8>, value: Vec<u8>) -> Result<()>;
+    fn delete(&mut self,key: &[u8]) -> Result<()>;
+    fn flush(&mut self) -> Result<()>;
 }
